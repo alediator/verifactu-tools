7d693480986d6373956fbc43005e47424c22b9de
Alejandro Díaz Torres <alejandro.diaz@grupomagnetia.com>
2025-07-16 13:43:11 +0200

    WIP

--- src/soap/registrarFacturas.ts
+++ src/soap/registrarFacturas.ts
@@ -1,8 +1,8 @@
 import { getVerifactuClient } from '../xml/getVerifactuClient';
-import type { RegistroFactura } from '../generated/sistemafacturacion/definitions/RegistroFactura';
+import type { RegFactuSistemaFacturacion } from '../generated/sistemafacturacion/definitions/RegFactuSistemaFacturacion';
 
-export async function registrarFacturas(input: RegistroFactura[], endpoint?: string) {
+export async function registrarFacturas(input: RegFactuSistemaFacturacion, endpoint?: string) {
   const client = await getVerifactuClient(endpoint);
-  const [{ Respuesta }] = await client.sfVerifactu.RegFactuSistemaFacturacionAsync({ RegistroFactura: input });
-  return Respuesta;
+  const respuesta = await client.RegFactuSistemaFacturacionAsync(input);
+  return respuesta;
 }
