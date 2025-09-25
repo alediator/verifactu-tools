97f637a35308b00eb5e53e018931e9ced4828c2c
Alejandro Díaz Torres <alejandro.diaz@grupomagnetia.com>
2025-09-25 12:03:01 +0200

    Minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // src/index.ts
-export { calcularHash } from './core/calcularHash';
-export { getVerifactuClient } from './xml/getVerifactuClient';
-export { generarXMLDesdeJson } from './xml/generarXMLDesdeJson';
-export { validarXMLContraXSD } from './utils/validateXml';
-export { formatFechaConZona } from './utils/fechaUtils';
-export { registrarFacturas } from './soap/registrarFacturas';
-export { consultarFacturas } from './soap/consultarFacturas';
+export { calcularHash } from './core/calcularHash.js';
+export { getVerifactuClient } from './xml/getVerifactuClient.js';
+export { generarXMLDesdeJson } from './xml/generarXMLDesdeJson.js';
+export { validarXMLContraXSD } from './utils/validateXml.js';
+export { formatFechaConZona } from './utils/fechaUtils.js';
+export { registrarFacturas } from './soap/registrarFacturas.js';
+export { consultarFacturas } from './soap/consultarFacturas.js';
